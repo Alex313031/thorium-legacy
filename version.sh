87b66eced12f93f75b3ebeebaf0223ff908ba0fc
Alexander Frick <Alex313031@gmail.com>
2023-10-14 09:03:13 -0500

    fix version.sh

--- version.sh
+++ version.sh
@@ -87,12 +87,12 @@ python3 tools/update_pgo_profiles.py --target=win32 update --gs-url-base=chromiu
 # python3 tools/update_pgo_profiles.py --target=mac update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 printf "\n" &&
 
-printf "${YEL}Downloading PGO Profile for V8 (when v8_enable_builtins_optimization = true)\n" &&
-printf "\n" &&
-tput sgr0 &&
+# printf "${YEL}Downloading PGO Profile for V8 (when v8_enable_builtins_optimization = true)\n" &&
+# printf "\n" &&
+# tput sgr0 &&
 
-python3 v8/tools/builtins-pgo/download_profiles.py --depot-tools=$HOME/depot_tools download &&
-printf "\n" &&
+# python3 v8/tools/builtins-pgo/download_profiles.py --depot-tools=$HOME/depot_tools download &&
+# printf "\n" &&
 
 cd ~/thorium-win7 &&
 
