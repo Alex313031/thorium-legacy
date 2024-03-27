e158829fc53724ab00ad8d71c6d512ca468e8f63
Alexander Frick <Alex313031@gmail.com>
2024-03-27 10:51:03 -0500

    .148 uprev

--- upstream_version.sh
+++ upstream_version.sh
@@ -18,7 +18,7 @@ try() { "$@" || die "${RED}Failed $*"; }
 # --help
 displayHelp () {
 	printf "\n" &&
-	printf "${bold}${YEL}Script to check out Chromium tag of current Thorium version.${c0}\n" &&
+	printf "${bold}${YEL}Script to check out Chromium tag of current Thorium-Win7 version.${c0}\n" &&
 	printf "\n"
 	printf "${RED}NOTE: You may need to run ${c0}${bold}./trunk.sh ${RED}before using this script!${c0}\n" &&
 	printf "\n"
@@ -36,27 +36,21 @@ else
     export CR_SRC_DIR
 fi
 
-THOR_VER="121.0.6167.206"
+CR_VER="122.0.6261.148"
 
-export THOR_VER &&
+export CR_VER &&
 
 printf "\n"
-printf "${GRE}Current Thorium version is:${c0} ${underline}$THOR_VER${c0}\n"
+printf "${GRE}Current Chromium version is:${c0} ${underline}$CR_VER${c0}\n"
 printf "\n"
-printf "${RED}NOTE: ${YEL}Checking out${CYA} tags/$THOR_VER ${YEL}in ${CR_SRC_DIR}...${c0}\n"
+printf "${RED}NOTE: ${YEL}Checking out${CYA} tags/$CR_VER ${YEL}in ${CR_SRC_DIR}...${c0}\n"
 printf "\n"
 
 cd ${CR_SRC_DIR} &&
 
-git checkout -f tags/$THOR_VER &&
+git checkout -f tags/$CR_VER &&
 
-cd ~/thorium &&
-
-# Use our artifacts hash
-cp -v src/build/vs_toolchain.py ${CR_SRC_DIR}/build/ &&
-
-cd ${CR_SRC_DIR} &&
-
+git clean -ffd &&
 git clean -ffd &&
 
 gclient sync --with_branch_heads --with_tags -f -R -D &&
@@ -64,28 +58,24 @@ gclient sync --with_branch_heads --with_tags -f -R -D &&
 gclient runhooks &&
 
 printf "\n"
-printf "${GRE}Chromium tree is checked out at tag: ${c0}$THOR_VER\n"
+printf "${GRE}Chromium tree is checked out at tag: ${c0}$CR_VER\n"
 printf "\n"
 	
-printf "${YEL}Downloading PGO Profiles for Linux, Windows, and Mac...\n" &&
+printf "${YEL}Downloading PGO Profiles for win64 and win32...\n" &&
 printf "\n" &&
 tput sgr0 &&
 
-python3 tools/update_pgo_profiles.py --target=linux update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
-
 python3 tools/update_pgo_profiles.py --target=win64 update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
+python3 tools/update_pgo_profiles.py --target=win32 update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 
-python3 tools/update_pgo_profiles.py --target=mac update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 printf "\n" &&
 
-printf "${YEL}Downloading PGO Profile for V8 (when v8_enable_builtins_optimization = true)\n" &&
+printf "${YEL}Downloading PGO Profile for V8 (for when v8_enable_builtins_optimization = true)\n" &&
 printf "\n" &&
 tput sgr0 &&
 
 python3 v8/tools/builtins-pgo/download_profiles.py --depot-tools=$HOME/depot_tools download &&
 printf "\n" &&
 
-cd ~/thorium &&
-
-printf "${GRE}Done! ${YEL}You can now run \'./setup.sh\'\n"
+printf "${GRE}Done!\n"
 tput sgr0
