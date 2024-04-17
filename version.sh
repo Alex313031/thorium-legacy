c190e23f398ab87672bbda87317fa4c271e3108b
Alexander Frick <Alex313031@gmail.com>
2024-04-17 08:21:56 -0500

    M122.0.6261.168 Final

--- version.sh
+++ version.sh
@@ -57,6 +57,7 @@ cp -v src/build/vs_toolchain.py ${CR_SRC_DIR}/build/ &&
 
 # Add //third_party/libjxl to DEPS
 cp -v thorium-libjxl/src/DEPS ${CR_SRC_DIR}/ &&
+cp -v thorium-libjxl/src/third_party/.gitignore ${CR_SRC_DIR}/third_party/ &&
 
 cd ${CR_SRC_DIR} &&
 
