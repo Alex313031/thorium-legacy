42aeab4726151a8c46f2d6899fb280b25f31ee9b
Alexander Frick <Alex313031@gmail.com>
2024-05-21 14:50:16 -0500

    lint flag

--- src/third_party/blink/common/features.cc
+++ src/third_party/blink/common/features.cc
@@ -876,7 +876,7 @@ BASE_FEATURE(kFileSystemUrlNavigation,
 
 BASE_FEATURE(kFileSystemUrlNavigationForChromeAppsOnly,
              "FileSystemUrlNavigationForChromeAppsOnly",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFilteringScrollPrediction,
              "FilteringScrollPrediction",
